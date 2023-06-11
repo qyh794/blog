14c7d5fb00f7459a33a6007c1b4b4656d44b9625
qyh794 <727280876@qq.com>
2023-06-11 11:26:03 +0800

    Create post.go

---
+++ models/post.go
@@ -0,0 +1,21 @@
+package models
+
+import "time"
+
+type Post struct {
+	CommunityID int64 `json:"community_id" db:"community_id" binding:"required"`
+	PostID      int64 `json:"post_id" db:"post_id"`
+	AuthorID    int64 `json:"author_id" db:"author_id"`
+	Title       string `json:"title" db:"title" binding:"required"`
+	Content     string `json:"content" db:"content" binding:"required"`
+	CreateTime  time.Time `json:"create_time" db:"create_time"`
+	UpdateTime  time.Time `json:"update_time" db:"update_time"`
+}
+
+
+type PostDetail struct {
+	AuthorName string `json:"author_name"`
+	*Post 
+	*CommunityDetail `json:"community"`
+}
+
