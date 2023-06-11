56a956c6c43cfaaa7766416b5161dde2ea20a943
qyh794 <727280876@qq.com>
2023-06-11 16:03:34 +0800

    Update post.go

--- models/post.go
+++ models/post.go
@@ -3,19 +3,18 @@ package models
 import "time"
 
 type Post struct {
-	CommunityID int64 `json:"community_id" db:"community_id" binding:"required"`
-	PostID      int64 `json:"post_id" db:"post_id"`
-	AuthorID    int64 `json:"author_id" db:"author_id"`
-	Title       string `json:"title" db:"title" binding:"required"`
-	Content     string `json:"content" db:"content" binding:"required"`
+	CommunityID int64     `json:"community_id" db:"community_id" binding:"required"`
+	PostID      int64     `json:"post_id" db:"post_id"`
+	AuthorID    int64     `json:"author_id" db:"author_id"`
+	Title       string    `json:"title" db:"title" binding:"required"`
+	Content     string    `json:"content" db:"content" binding:"required"`
 	CreateTime  time.Time `json:"create_time" db:"create_time"`
 	UpdateTime  time.Time `json:"update_time" db:"update_time"`
 }
 
-
 type PostDetail struct {
 	AuthorName string `json:"author_name"`
-	*Post 
+	VoteNum    int64  `json:"vote_num"`
+	*Post
 	*CommunityDetail `json:"community"`
 }
-
