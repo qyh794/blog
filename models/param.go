494084b3c52003e86c1060f19cc49bf1e73d0b37
qyh794 <727280876@qq.com>
2023-06-11 16:03:31 +0800

    Update param.go

--- models/param.go
+++ models/param.go
@@ -1,8 +1,13 @@
 package models
 
+const (
+	OrderTime  = "time"
+	OrderScore = "score"
+)
+
 type ParamSignUp struct {
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Username   string `json:"username" binding:"required"`
+	Password   string `json:"password" binding:"required"`
 	RePassword string `json:"re_password" binding:"required,eqfield=Password"`
 }
 
@@ -12,6 +17,12 @@ type ParamLogin struct {
 }
 
 type ParamVoteData struct {
-	PostID string `json:"post_id" binding:"required"`
-	Direction int8 `json:"direction,string" binding:"oneof=-1 0 1"`
+	PostID    string `json:"post_id" binding:"required"`
+	Direction int8   `json:"direction,string" binding:"oneof=-1 0 1"`
+}
+
+type ParamPostList struct {
+	Page  int64
+	Size  int64
+	Order string
 }
