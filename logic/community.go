e9c2a726c6b3fb8b52710118c072c380f59f99e9
qyh794 <727280876@qq.com>
2023-07-05 23:51:20 +0800

    Update community.go

--- logic/community.go
+++ logic/community.go
@@ -1,14 +1,14 @@
 package logic
 
 import (
-	"blog/dao/mysql"
+	"blog/dao/postgresql"
 	"blog/models"
 )
 
 func CommunityList() ([]*models.Community, error) {
-	return mysql.CommunityList()
+	return postgresql.CommunityList()
 }
 
 func GetCommunityDetailByID(id int64) (models.CommunityDetail, error) {
-	return mysql.GetCommunityDetailByID(id)
+	return postgresql.GetCommunityDetailByID(id)
 }
